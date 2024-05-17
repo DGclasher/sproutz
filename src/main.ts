3cc08f8a3b516a52d7b79d4ce998add2d2b1421a
DGclasher <73327721+DGclasher@users.noreply.github.com>
2024-05-17 12:16:22 +0530

    add allowed origins

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://the-wildflowers.vercel.app',
+];
 async function bootstrap() {
   const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
   });
   await app.listen(port, () => {
     console.log(`Server running on ${port}`);
