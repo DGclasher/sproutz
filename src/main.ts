6918f9679c2539adb410c4dd1b3446524e482af1
DGclasher <73327721+DGclasher@users.noreply.github.com>
2024-05-17 11:53:22 +0530

    fix cors 3

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: true,
-    methods: ['GET', 'POST'],
-    credentials: true,
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(port, () => {
     console.log(`Server running on ${port}`);
   });
